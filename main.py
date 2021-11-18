3ec498dad0a97e7f4a33686e968e3da9ca6b1359
Guy Wilson <Guywilsonjr@gmail.com>
2021-11-17 20:12:15 -0800

    addcors

--- main.py
+++ main.py
@@ -11,7 +11,24 @@ from simulator import GameState
 app = FastAPI()
 sim = Simulator()
 db = dm.start_new_db()
+from fastapi.middleware.cors import CORSMiddleware
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def healthcheck() -> Dict[str, str]:
