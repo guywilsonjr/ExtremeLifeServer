6b99b9a7ba9e2b4f7a3f842f1a3db88155bf99de
denmcca <denmcca@gmail.com>
2021-12-18 11:01:33 -0800

    start.sh script now working. use 'sudo bash ./start.sh'

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
+
+
 echo Starting game API
-uvicorn main:app --host=0.0.0.0 --ssl-keyfile /etc/letsencrypt/live/comp680elgame.tk/privkey.pem --ssl-certfile /etc/letsencrypt/live/comp680elgame.tk/fullchain.pem --port 443 --debug --workers 3
+screen -dmS "gameAPI" bash -c '/home/ubuntu/ExtremeLifeServer/venv/bin/uvicorn main:app --host=0.0.0.0 --ssl-keyfile /etc/letsencrypt/live/comp680elgame.tk/privkey.pem --ssl-certfile /etc/letsencrypt/live/comp680elgame.tk/fullchain.pem --port 443 --debug --workers 3; exec bash'
+
+
 echo Starting chat API
-uvicorn chatapi:app --host=0.0.0.0 --port=8000 --debug
+screen -dmS "chatAPI" bash -c '/home/ubuntu/ExtremeLifeServer/venv/bin/uvicorn chatapi:app --host=0.0.0.0 --ssl-keyfile /etc/letsencrypt/live/comp680elgame.tk/privkey.pem --ssl-certfile /etc/letsencrypt/live/comp680elgame.tk/fullchain.pem --port=8000 --debug; exec bash'
